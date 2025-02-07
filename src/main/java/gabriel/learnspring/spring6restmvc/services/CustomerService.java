b27e438541e224651e4e2c8587636631bbf536c3
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-02-07 20:03:40 -0300

    feat: add method to list all customers
    * Returns a new array list that contains all customers.

--- src/main/java/gabriel/learnspring/spring6restmvc/services/CustomerService.java
+++ src/main/java/gabriel/learnspring/spring6restmvc/services/CustomerService.java
@@ -2,9 +2,12 @@ package gabriel.learnspring.spring6restmvc.services;
 
 import gabriel.learnspring.spring6restmvc.model.Customer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CustomerService {
+
+    List<Customer> listCustomers();
 
     Customer getCustomerById(UUID id);
 }
--- src/main/java/gabriel/learnspring/spring6restmvc/services/CustomerServiceImpl.java
+++ src/main/java/gabriel/learnspring/spring6restmvc/services/CustomerServiceImpl.java
@@ -5,22 +5,65 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+    private Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl(){
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Carlos Pereira")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Celso Russoumano")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Luan Peres")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        Customer customer4 = Customer.builder()
+                .id(UUID.randomUUID())
+                .customerName("Neymar Júnior")
+                .version(1)
+                .createdDate(LocalDateTime.now())
+                .lastModifiedDate(LocalDateTime.now())
+                .build();
+
+        customerMap.put(customer1.getId(), customer1);
+        customerMap.put(customer2.getId(), customer2);
+        customerMap.put(customer3.getId(), customer3);
+        customerMap.put(customer4.getId(), customer4);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
     @Override
     public Customer getCustomerById(UUID id) {
 
         log.debug("Get Customer By Id in service. Id: {}", id.toString());
 
-        return Customer.builder()
-                .id(id)
-                .customerName("Carlos")
-                .version(1)
-                .createdDate(LocalDateTime.now())
-                .lastModifiedDate(LocalDateTime.now())
-                .build();
+        return customerMap.get(id);
     }
 }
