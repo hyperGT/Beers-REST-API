1ac264f4d772bc2a127eb351bde2a75837303910
Gabriel Teixeira Charles da Silva <charlesgabriel102@gmail.com>
2025-02-07 20:04:18 -0300

    feat: add endpoint to list all customers
    * Implements a GET endpoint at /customers to retrieve a list of all customers.
    * Returns a JSON array containing customer data.

--- src/main/java/gabriel/learnspring/spring6restmvc/controller/CustomerController.java
+++ src/main/java/gabriel/learnspring/spring6restmvc/controller/CustomerController.java
@@ -4,20 +4,31 @@ import gabriel.learnspring.spring6restmvc.model.Customer;
 import gabriel.learnspring.spring6restmvc.services.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
     private final CustomerService customerService;
 
-    public Customer getCustomerById(UUID id){
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers(){
+        return customerService.listCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable UUID customerId){
 
         log.debug("Get Customer By Id in Controller");
 
-        return customerService.getCustomerById(id);
+        return customerService.getCustomerById(customerId);
     }
 }
